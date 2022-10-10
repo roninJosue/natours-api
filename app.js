391296f405b8e8cef5841f14851926298f7c1547
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-10 17:15:07 -0600

    Get tour by id

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -17,7 +19,44 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-const port = 3000;
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id = null } = req.params;
+
+  const tour = tours.find((el) => el.id === +id);
+  if (!tour)
+    return res.status(404).send({ status: 'fail', message: 'Invalid ID' });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+const port = 3010;
+
 app.listen(port, () => {
-  console.log(`running on prt ${port}`);
+  console.log(`running on port ${port}`);
 });
