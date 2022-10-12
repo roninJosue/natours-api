6c2828aa86ae16092ec2bfd3d3d8acada69c4a1c
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-11 22:04:28 -0600

    Add morgan middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
