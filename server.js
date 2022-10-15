aaeefebdd8210123ee1377bd979aa1fcb2cdc771
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-14 22:47:39 -0600

    Set environment variables

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3010;
+const port = process.env.PORT || 3010;
 
 app.listen(port, () => {
   console.log(`running on port ${port}`);
