aefe2d7ad2673f95a7057d9bba3847f50803c45e
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-27 23:10:44 -0600

    Get tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('./../modules/tourModel');
+const Tour = require('../modules/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
@@ -18,17 +18,21 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  const { id = null } = req.params;
-
-  // const tour = tours.find((el) => el.id === +id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
