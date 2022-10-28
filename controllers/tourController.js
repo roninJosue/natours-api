c54bf20163ba9efa8c0fd6b1642f91e021bc72fb
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-27 23:02:26 -0600

    Get all tours from database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,21 @@
 const Tour = require('./../modules/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    //results: tours.length,
-    //data: {
-    //  tours,
-    //},
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
