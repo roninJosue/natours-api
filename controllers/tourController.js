27bccd51ae41596d1dafc10e124b15a6e9e43b49
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-31 22:00:23 -0600

    Add sorting to the get all tours API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,12 +9,19 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     const tours = await query;
 
