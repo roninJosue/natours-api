8cbc0059183eafd13e43fbc326fdbe105780c715
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-09 21:47:17 -0600

    Test validator isAlpha in tour name field

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxLength: [40, 'A tour name must have les or equal than 40 characters'],
       minLength: [10, 'A tour name must have more or equal than 10 characters'],
+      validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
