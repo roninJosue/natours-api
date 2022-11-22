d5316d5745d80f1352e7e297f3ef15941b8bdd70
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-21 21:50:11 -0600

    Send different error message depending it if PROD or DEV

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('ERROR', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
