a2afc2dd55ce931c9d1263fcecdc009dde5764c3
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-23 21:56:33 -0600

    Handle duplicate error message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["']).*?\1/)[0];
+  const message = `Duplicate field value: ${value}.Please use another value`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,7 +46,9 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     error.name = err.name;
+    error.errmsg = err.errmsg;
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
