4eac36c9fc819b4fcc83c98b96d379eefb76190e
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-24 17:22:23 -0600

    Delete console log

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -53,7 +53,6 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     error.name = err.name;
     error.errmsg = err.errmsg;
-    console.log(error);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
