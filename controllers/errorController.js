89ab76fdd8b5746ef3cdf0a21d225922d39b01b7
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-24 17:21:46 -0600

    Handle Mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -12,6 +12,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -47,8 +53,11 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     error.name = err.name;
     error.errmsg = err.errmsg;
+    console.log(error);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
