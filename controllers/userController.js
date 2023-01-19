3ad1276e3e140e3cd9b86f79d66688ac1e409877
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-01-18 20:38:17 -0600

    Implement deleteMe method in controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,6 +49,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', data: { user: updatedUser } });
 });
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
