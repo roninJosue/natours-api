e86df5a54e2b2454431331725e79750af693614d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-01-22 21:28:23 -0600

    Embed tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -100,6 +102,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +117,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() and not for update
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
