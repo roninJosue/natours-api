f69d3539a7d4dc6abe660c1934c6629715a22407
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-01-25 21:17:33 -0600

    Populate tour and user from a specific review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,8 +35,11 @@ const reviewSchema = new mongoose.Schema(
 
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
-    path: 'tour user',
-    select: '-__v',
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
   });
   next();
 });
