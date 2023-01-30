8fc366a3723379eba9ec43039c9b4eed525ff65d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-01-30 17:24:24 -0600

    Get tourId, userId from body or query param

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,9 @@ const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewModel');
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user._id;
+
   const newReview = await Review.create({ ...req.body, user: req.user._id });
   res.status(201).send({
     status: 'success',
