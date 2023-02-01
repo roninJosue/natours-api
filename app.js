9be3001840358b48c0a15af60be4891c4cf78550
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-01 08:38:09 -0600

    Use custom morgan middleware

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 
+const morganMiddleware = require('./middlewares/morgan.middleware');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -16,9 +16,9 @@ const app = express();
 
 app.use(helmet());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+//if (process.env.NODE_ENV === 'development') {
+app.use(morganMiddleware);
+//}
 
 const limiter = rateLimit({
   max: 100,
