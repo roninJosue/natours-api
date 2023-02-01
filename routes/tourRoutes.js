be10edc8392b12d1c6204134cb169e6c61784f41
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-01 08:42:41 -0600

    Nest review routes in tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 //router.param('id', tourController.checkID);
@@ -28,12 +28,6 @@ router
     tourController.deleteTour
   );
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-  );
+router.use('/:tourId/reviews', reviewRouter);
 
 module.exports = router;
