87ab9af83cbbc20e66ad9a71f45d5003e6a8b1b6
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-02 22:06:46 -0600

    Add updateOne and createOne generic functions

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -15,3 +15,35 @@ exports.deleteOne = (Model) =>
       data: null,
     });
   });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const { id = null } = req.params;
+    const doc = await Model.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).send({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
