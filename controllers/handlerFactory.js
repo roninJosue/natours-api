f6d9b573d6ff79fbf67f98bdbb02c09fa2d5bccf
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-02 21:40:00 -0600

    Create delete one generic function

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,17 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const { id = null } = req.params;
+    const doc = await Model.findByIdAndDelete(id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
