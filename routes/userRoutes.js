9c4150ddcf2e28a526bd6f86fae6a2a9e7b7c9d8
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-13 21:21:36 -0600

    Add authentication and authorization to user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,20 +10,24 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.use(authController.protect);
+
 router.patch(
   '/updateMyPassword',
-  authController.protect,
+
   authController.updatePassword
 );
 
 router.get(
   '/me',
-  authController.protect,
+
   userController.getMe,
   userController.getUser
 );
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
