435a1bf8f738d073c3b334e9c50d883143b3c289
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-22 21:38:44 -0600

    Add tour within route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,6 +20,10 @@ router
   );
 
 router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router
   .route('/')
   .get(authController.protect, tourController.getAllTours)
   .post(
