7d23ffb667f9f48e1c5f6c38d48b7faf92666059
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-22 21:38:30 -0600

    Create method for get tours within a distance

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -94,4 +95,32 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
       plan,
     },
   });
+});
+
+// 34.119415, -118.135390
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'please provide latitude and longitude ion the format lat,lng',
+        400
+      )
+    );
+  }
+
+  const tour = await Tour.find({
+    startLocation: {
+      $geoWithin: {
+        $centerSphere: [[lng, lat], radius],
+      },
+    },
+  });
+
+  res
+    .status(200)
+    .json({ status: 'success', results: tour.length, data: { data: tour } });
 });
