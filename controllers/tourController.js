d638c833cba26c41c14568a2987bc50017c7ef8d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-23 20:50:12 -0600

    Add pipeline to get distances of the tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -106,7 +106,7 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   if (!lat || !lng) {
     next(
       new AppError(
-        'please provide latitude and longitude ion the format lat,lng',
+        'please provide latitude and longitude in the format lat,lng',
         400
       )
     );
@@ -123,4 +123,40 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   res
     .status(200)
     .json({ status: 'success', results: tour.length, data: { data: tour } });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'please provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({ status: 'success', data: { data: distances } });
 });
