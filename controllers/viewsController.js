018d11248ed5874ed8b2ed6314af54356f2d5abe
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-02-27 21:44:31 -0600

    Pass all tours to the overview template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,13 @@
-exports.getOverview = (req, res, next) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res, next) => {
   res.status(200).render('tour', {
