c21fe98324b21884c750f2150a311e82dd71d100
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-03-02 22:02:38 -0600

    Add login route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const viewsControllers = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/', viewsControllers.getOverview);
-router.get('/tour/:slug', viewsControllers.getTour);
+router.get('/tour/:slug', authController.protect, viewsControllers.getTour);
+router.get('/login', viewsControllers.getLoginForm);
 
 module.exports = router;
