685563674ae2f1b542c4dbfec44da3523a17a55d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-03-07 08:24:08 -0600

    Add logout function

--- public/js/login.js
+++ public/js/login.js
@@ -29,3 +29,17 @@ export const login = async (email, password) => {
     console.log(error);
   }
 };
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3088/api/v1/users/logout',
+    });
+    if (res.data.status === 'success') {
+      location.reload(true);
+    }
+  } catch (error) {
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
