6f3238d69301384660856d170328c91267771ca8
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-03-08 20:54:24 -0600

    Add update user data form

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 const loginForm = document.querySelector('.form--login');
+const updateForm = document.querySelector('.form-user-data');
 const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (loginForm) {
@@ -10,6 +12,16 @@ if (loginForm) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
+  });
+}
+
+if (updateForm) {
+  document.querySelector('.form-user-data').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('UPDATE');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
   });
 }
 
