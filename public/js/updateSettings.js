b4d1e96237b00def1859a14aef1e034f1fb838a3
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-03-08 21:25:37 -0600

    Change update settings function

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const updateData = async (name, email) => {
+export const updateSettings = async (data, type) => {
   try {
-    const res = await axios.patch(
-      'http://127.0.0.1:3088/api/v1/users/updateMe',
-      {
-        name,
-        email,
-      }
-    );
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3088/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3088/api/v1/users/updateMe';
+    const res = await axios.patch(url, {
+      ...data,
+    });
     if (res.data.status === 'success') {
-      showAlert('success', 'Updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (error) {
     showAlert('error', error.response.data.message);
