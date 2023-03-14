689afbc8c67b9fdb411a8e31a0e712a1c8247c76
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-03-13 20:50:39 -0600

    Change patch method to axios method

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -7,8 +7,10 @@ export const updateSettings = async (data, type) => {
       type === 'password'
         ? 'http://127.0.0.1:3088/api/v1/users/updateMyPassword'
         : 'http://127.0.0.1:3088/api/v1/users/updateMe';
-    const res = await axios.patch(url, {
-      ...data,
+    const res = await axios({
+      url,
+      method: 'PATCH',
+      data,
     });
     if (res.data.status === 'success') {
       showAlert('success', `${type.toUpperCase()} updated successfully!`);
