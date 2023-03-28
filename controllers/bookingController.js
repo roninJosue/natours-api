649377c1758ddcc50b6b7a6206f21cd482e490cc
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-03-27 20:59:35 -0600

    Create method for checkout session

---
+++ controllers/bookingController.js
@@ -0,0 +1,35 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KET);
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          unit_amount: tour.price * 100,
+          currency: 'usd',
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
+    mode: 'payment',
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
