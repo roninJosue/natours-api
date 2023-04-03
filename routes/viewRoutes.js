ef5f7e1d8ffbd077e697f045a68c59dc9dc9a307
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2023-04-03 09:11:10 -0600

    add 'my-tours' route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const viewsControllers = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsControllers.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsControllers.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsControllers.getTour);
 router.get('/login', authController.isLoggedIn, viewsControllers.getLoginForm);
 router.get('/me', authController.protect, viewsControllers.getAccount);
+router.get('/my-tours', authController.protect, viewsControllers.getMyTours);
 
 router.post(
   '/submit-user-data',
